1e9ff4d15caef9dd2380fb65e502bc79f463214f
wadoro <wadoro@msn.com>
2020-11-03 16:27:25 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -12,10 +12,10 @@ MU_SUFFIX = 'microsoft.com'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
+API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
 WEBAPI_URL = 'https://zhaoj.in'
-WEBAPI_TOKEN = 'glzjin'
+WEBAPI_TOKEN = 'NimaQu'
 
 # mudb
 MUDB_FILE = 'mudb.json'
