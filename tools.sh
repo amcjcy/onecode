c3ecf6e6e8492b2face2303e499c17d0b8f6635b
amcjcy <82363754+amcjcy@users.noreply.github.com>
2021-10-31 11:03:26 +0800

    Add files via upload

--- tools.sh
+++ tools.sh
@@ -41,37 +41,24 @@ sed -i '/net.ipv4.udp_wmem_min/d' /etc/sysctl.conf
 sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf
 sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf
 cat >> /etc/sysctl.conf << EOF
-net.core.default_qdisc = fq
-net.ipv4.tcp_congestion_control = bbr
-net.ipv4.tcp_fastopen = 3
-fs.file-max = 51200
-net.core.rmem_max=16777216
-net.core.wmem_max=16777216
-net.core.rmem_default = 65536
-net.core.wmem_default = 65536
-net.ipv4.tcp_rmem=4096 65536 16777216
-net.ipv4.tcp_wmem=4096 65536 16777216
-net.ipv4.udp_rmem_min=8192
-net.ipv4.udp_wmem_min=8192
-net.ipv4.tcp_mtu_probing=0
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
 net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
 net.ipv4.tcp_rfc1337=1
 net.ipv4.tcp_sack=1
 net.ipv4.tcp_fack=1
 net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_adv_win_scale=2
 net.ipv4.tcp_moderate_rcvbuf=1
-net.core.netdev_max_backlog = 4096
-net.core.somaxconn = 4096
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 4096
-net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_rmem=4096 65536 16777216
+net.ipv4.tcp_wmem=4096 65536 16777216
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+net.ipv4.udp_rmem_min=8192
+net.ipv4.udp_wmem_min=8192
+net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr
 EOF
 sysctl -p && sysctl --system
 }
@@ -108,6 +95,11 @@ sysctl -p && sysctl --system
 ulimit_tune(){
 
 echo "1000000" > /proc/sys/fs/file-max
+sed -i '/fs.file-max/d' /etc/sysctl.conf
+cat >> '/etc/sysctl.conf' << EOF
+fs.file-max=1000000
+EOF
+
 ulimit -SHn 1000000 && ulimit -c unlimited
 echo "root     soft   nofile    1000000
 root     hard   nofile    1000000
@@ -157,16 +149,23 @@ DefaultLimitNOFILE=65535
 DefaultLimitNPROC=65535
 EOF
 
-systemd --daemon-reload
+systemctl daemon-reload
 
 }
 
 bbr(){
-  wget -N "http://sh.neko.sbs:1111/bbr/bbr.sh" -O bbr.sh && bash bbr.sh
+
+if uname -r|grep -q "^5."
+then
+    echo "已经是 5.x 内核，不需要更新"
+else
+    wget -N "http://sh.nekoneko.cloud/bbr/bbr.sh" -O bbr.sh && bash bbr.sh
+fi
+  
 }
 
 Update_Shell(){
-  wget -N "http://sh.neko.sbs:1111/tools.sh" -O tools.sh && chmod +x tools.sh && ./tools.sh
+  wget -N "http://sh.nekoneko.cloud/tools.sh" -O tools.sh && chmod +x tools.sh && ./tools.sh
 }
 
 get_opsy() {
